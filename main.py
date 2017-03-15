496250118c41c234d42a51a5e83793e3014e2cd9
sugyan <sugi1982@gmail.com>
2017-03-15 22:33:57 +0900

    diagnose tweet text

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 import logging
+import random
 import tweepy
 import requests_toolbelt.adapters.appengine
 
 from flask import Flask, render_template, request
+from google.cloud import language
 
 requests_toolbelt.adapters.appengine.monkeypatch()
 app = Flask(__name__)
@@ -14,10 +16,10 @@ def collect_tweets(target):
     auth = tweepy.AppAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
     api = tweepy.API(auth)
     try:
-        texts = []
+        tweets = []
         for status in api.user_timeline(target, include_rts=False, count=100, exclude_replies=True):
-            texts.append(status.text)
-        return '\n'.join(texts)
+            tweets.append(status.text)
+        return '\n'.join(tweets)
     except tweepy.error.TweepError:
         return ''
 
@@ -26,7 +28,17 @@ def analyze(target):
     text = collect_tweets(target)
     if text == '':
         return '取得できませんでした'
-    return text
+
+    language_client = language.Client()
+    document = language_client.document_from_text(text)
+    annotated = document.annotate_text(include_syntax=False)
+
+    entity_names = set([entity.name.encode('utf-8') for entity in annotated.entities if entity.entity_type != 'OTHER'])
+    total_score = sum([sentence.sentiment.magnitude * sentence.sentiment.score for sentence in annotated.sentences])
+    return '@{} さんは {} などを呟いています。全体的なポジティブ度合いは… {}！'.format(
+        target,
+        ''.join(['「{}」'.format(x) for x in random.sample(entity_names, 7)]),
+        total_score)
 
 
 @app.route('/diagnose', methods=['POST'])
